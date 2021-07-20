297266930ed0de3a843755d33f2541bc2556df0d
benRenard <benjamin.renard@inrae.fr>
2021-07-20 16:59:45 +0200

    Implemented the use of an optional "progress bar" file monitoring the current MCMC iteration through a simple i/n written to file.

--- src/MCMCStrategy_tools.f90
+++ src/MCMCStrategy_tools.f90
@@ -32,7 +32,7 @@ Contains
 
 subroutine Adaptive_Metro(f,x,fx,fAux,covar,scale,nAdapt,nCycles,&
                 MinMoveRate,MaxMoveRate,DownMult,UpMult,&
-                OutFile,headers,err,mess)
+                OutFile,MonitorFile,headers,err,mess)
 
 !^**********************************************************************
 !^* Purpose: Metropolis with adaptive scale factor
@@ -61,13 +61,12 @@ subroutine Adaptive_Metro(f,x,fx,fAux,covar,scale,nAdapt,nCycles,&
 !^*    2.
 !^*    3.
 !^**********************************************************************
-use utilities_dmsl_kit, only:number_string
-!Use RFortran
+use utilities_dmsl_kit, only:number_string,getSpareUnit
 
 real(mrk),intent(inout)::x(:),covar(:,:),scale
 integer(mik),intent(in)::nAdapt,nCycles
 real(mrk),intent(in)::MinMoveRate,MaxMoveRate,DownMult,UpMult
-character(*), intent(in),optional::OutFile,headers(:)
+character(*), intent(in),optional::OutFile,MonitorFile,headers(:)
 real(mrk),intent(out)::fx
 real(mrk),intent(out),optional::fAux(:)
 integer(mik), intent(out)::err
@@ -77,7 +76,7 @@ logical::feas,isnull,again
 character(100), allocatable::head(:), fAuxHead(:)
 logical::move
 real(mrk)::moverate
-integer(mik)::n,i,p,k,compt
+integer(mik)::n,i,p,k,compt,untOut,untMonitor
 real(mrk)::postlist(nAdapt)
 character(250)::fmt
 
@@ -96,11 +95,13 @@ end interface
 !Handle Output file headers
 n=size(x,dim=1)
 if(present(OutFile)) then
-    open(unit=1,file=OutFile,status='replace')
+    call getSpareUnit(untOut,err,mess)
+    If(err>0) then;mess='Adaptive_Metro:'//trim(mess);return;endif
+    open(unit=untOut,file=OutFile,status='replace')
     if(present(headers)) then ! trust user - no size check at the moment
         k=size(headers)
         fmt='(' // trim(number_string(k)) // '(A13," "))'
-        write(1,trim(fmt)) headers
+        write(untOut,trim(fmt)) headers
     else
         if(allocated(head)) deallocate(head)
         allocate(head(n))
@@ -115,49 +116,62 @@ if(present(OutFile)) then
                 fAuxhead(i)='fAux'//trim(number_string(i))
             enddo
             fmt='(' // trim(number_string(n+1+p)) // '(A13," "))'
-            write(1,trim(fmt)) head(:),'Objectivefunction',fAuxHead(:)
+            write(untOut,trim(fmt)) head(:),'Objectivefunction',fAuxHead(:)
             deallocate(fAuxHead)
         else
             fmt='(' // trim(number_string(n+1)) // '(A13," "))'
-            write(1,trim(fmt)) head(:),'Objectivefunction'
+            write(untOut,trim(fmt)) head(:),'Objectivefunction'
         endif
         deallocate(head)
     endif
 endif
 
+!Handle Monitoring file
+if(present(MonitorFile)) then
+    call getSpareUnit(untMonitor,err,mess)
+    If(err>0) then
+        mess='Adaptive_Metro:'//trim(mess)
+        if(present(OutFile)) close(untOut)
+        return
+    endif
+    open(unit=untMonitor,file=MonitorFile,status='replace')
+endif
+
 ! Check starting value
 call f(x=x,feas=feas,isnull=isnull,fx=fx,fAux=fAux,err=err,mess=mess)
 If(err>0) then
-    mess='Adaptive_Metro'//trim(mess)
-    if(present(OutFile)) close(1)
+    mess='Adaptive_Metro:'//trim(mess)
+    if(present(OutFile)) close(untOut)
+    if(present(MonitorFile)) close(untMonitor)
     return
 endif
 if((.not.feas).or.isnull) then
     err=1;mess='Adaptive_Metro:Fatal:Unfeasible starting point'
-    if(present(OutFile)) close(1)
+    if(present(OutFile)) close(untOut)
+    if(present(MonitorFile)) close(untMonitor)
     return
 endif
 if(present(OutFile)) then
     if(present(fAux)) then
         fmt='(' // trim(number_string(n+1+p)) // 'e14.6)'
-        write (1,trim(fmt)) x,fx,fAux
+        write (untOut,trim(fmt)) x,fx,fAux
     else
         fmt='(' // trim(number_string(n+1)) // 'e14.6)'
-        write (1,trim(fmt)) x,fx
+        write (untOut,trim(fmt)) x,fx
     endif
 endif
 
-!ok = Rinit();ok=Reval('X11()')
-
 again=.true.;compt=0
+if(present(MonitorFile)) call writeMonitor(untMonitor,nAdapt*compt,nAdapt*nCycles)
 do while(again)
     !Start sampling
     moverate=0._mrk
     do i=1,nAdapt
         call Metro_GaussJump(f,x,fx,fAux,(scale**2)*covar,move,err,mess)
         If(err>0) then
-            mess='Adaptive_Metro'//trim(mess)
-            if(present(OutFile)) close(1)
+            mess='Adaptive_Metro:'//trim(mess)
+            if(present(OutFile)) close(untOut)
+            if(present(MonitorFile)) close(untMonitor)
             return
         endif
         postlist(i)=fx
@@ -165,22 +179,15 @@ do while(again)
         if(present(OutFile)) then
             if(present(fAux)) then
                 fmt='(' // trim(number_string(n+1+p)) // 'e14.6)'
-                write (1,trim(fmt)) x,fx,fAux
+                write (untOut,trim(fmt)) x,fx,fAux
             else
                 fmt='(' // trim(number_string(n+1)) // 'e14.6)'
-                write (1,trim(fmt)) x,fx
+                write (untOut,trim(fmt)) x,fx
             endif
         endif
         !Update Move Rates
         if(move) moverate=moverate+1._mrk/nAdapt
     enddo
-!    ok=Reval('layout(c(1, 2))')
-!    ok=Rput('mr', moverate)
-!    ok=Rput('compt', compt+1)
-!    ok=Reval('mrl[compt]<-mr')
-!    ok=Reval('matplot(mrl,type="l", xlab="Parameter", ylab="Move Rate", main="Move Rates", ylim=c(0, 1))')
-!    ok=Rput('post', postlist)
-!    ok=Reval('matplot(post,type="l", xlab="Iteration", ylab="Log-posterior", main="Log-posterior")')
 
     ! Adapt Jump standard deviation
     if(moverate<=MinMoveRate) scale=scale*DownMult
@@ -192,10 +199,11 @@ do while(again)
         write(*,*) 'Again?'
         read(*, *) again
     endif
+    if(present(MonitorFile)) call writeMonitor(untMonitor,nAdapt*compt,nAdapt*nCycles)
 enddo
 
-if(present(OutFile)) close(1)
-!ok = Rclose()  ! End R session
+if(present(OutFile)) close(untOut)
+if(present(MonitorFile)) close(untMonitor)
 
 end subroutine Adaptive_Metro
 
@@ -203,10 +211,10 @@ end subroutine Adaptive_Metro
 
 subroutine Adaptive_Metro_OAAT(f,x,fx,fAux,std,nAdapt,nCycles,&
                 MinMoveRate,MaxMoveRate,DownMult,UpMult,&
-                OutFile,headers,err,mess)
+                OutFile,MonitorFile,headers,err,mess)
 
 !^**********************************************************************
-!^* Purpose: 
+!^* Purpose:
 !^**********************************************************************
 !^* Programmer: Ben Renard, University of Newcastle
 !^**********************************************************************
@@ -231,13 +239,12 @@ subroutine Adaptive_Metro_OAAT(f,x,fx,fAux,std,nAdapt,nCycles,&
 !^*    2.
 !^*    3.
 !^**********************************************************************
-use utilities_dmsl_kit, only:number_string
-!Use RFortran
+use utilities_dmsl_kit, only:number_string,getSpareUnit
 
 real(mrk),intent(inout)::x(:),std(:)
 integer(mik),intent(in)::nAdapt,nCycles
 real(mrk),intent(in)::MinMoveRate,MaxMoveRate,DownMult,UpMult
-character(*), intent(in),optional::OutFile,headers(:)
+character(*), intent(in),optional::OutFile,MonitorFile,headers(:)
 real(mrk),intent(out)::fx
 real(mrk),intent(out),optional::fAux(:)
 integer(mik), intent(out)::err
@@ -247,7 +254,7 @@ logical::feas,isnull,again
 character(100), allocatable::head(:), fAuxHead(:)
 logical,allocatable::move(:)
 real(mrk),allocatable::moverate(:)
-integer(mik)::n,i,p,k,compt
+integer(mik)::n,i,p,k,compt,untOut,untMonitor
 real(mrk)::postlist(nAdapt)
 character(250)::fmt
 
@@ -268,11 +275,13 @@ n=size(x,dim=1)
 if(present(fAux)) then;p=size(fAux,dim=1);else;p=0;endif
 
 if(present(OutFile)) then
-    open(unit=1,file=OutFile,status='replace')
+    call getSpareUnit(untOut,err,mess)
+    If(err>0) then;mess='Adaptive_Metro_OAAT:'//trim(mess);return;endif
+    open(unit=untOut,file=OutFile,status='replace')
     if(present(headers)) then ! trust user - no size check at the moment
         k=size(headers)
         fmt='(' // trim(number_string(k)) // '(A13," "))'
-        write(1,trim(fmt)) headers
+        write(untOut,trim(fmt)) headers
     else
         if(allocated(head)) deallocate(head)
         allocate(head(n))
@@ -286,35 +295,48 @@ if(present(OutFile)) then
                 fAuxhead(i)='fAux'//trim(number_string(i))
             enddo
             fmt='(' // trim(number_string(n+1+p)) // '(A13," "))'
-            write(1,trim(fmt)) head(:),'Objectivefunction',fAuxHead(:)
+            write(untOut,trim(fmt)) head(:),'Objectivefunction',fAuxHead(:)
             deallocate(fAuxHead)
         else
             fmt='(' // trim(number_string(n+1)) // '(A13," "))'
-            write(1,trim(fmt)) head(:),'Objectivefunction'
+            write(untOut,trim(fmt)) head(:),'Objectivefunction'
         endif
         deallocate(head)
     endif
 endif
 
+!Handle Monitoring file
+if(present(MonitorFile)) then
+    call getSpareUnit(untMonitor,err,mess)
+    If(err>0) then
+        mess='Adaptive_Metro:'//trim(mess)
+        if(present(OutFile)) close(untOut)
+        return
+    endif
+    open(unit=untMonitor,file=MonitorFile,status='replace')
+endif
+
 ! Check starting value
 call f(x=x,feas=feas,isnull=isnull,fx=fx,fAux=fAux,err=err,mess=mess)
 If(err>0) then
-    mess='Adaptive_Metro_OAAT'//trim(mess)
-    if(present(OutFile)) close(1)
+    mess='Adaptive_Metro_OAAT:'//trim(mess)
+    if(present(OutFile)) close(untOut)
+    if(present(MonitorFile)) close(untMonitor)
     return
 endif
 if((.not.feas).or.isnull) then
     err=1;mess='Adaptive_Metro_OAAT:Fatal:Unfeasible starting point'
-    if(present(OutFile)) close(1)
+    if(present(OutFile)) close(untOut)
+    if(present(MonitorFile)) close(untMonitor)
     return
 endif
 if(present(OutFile)) then
     if(present(fAux)) then
         fmt='(' // trim(number_string(n+1+p)) // 'e14.6)'
-        write (1,trim(fmt)) x,fx,fAux
+        write (untOut,trim(fmt)) x,fx,fAux
     else
         fmt='(' // trim(number_string(n+1)) // 'e14.6)'
-        write (1,trim(fmt)) x,fx
+        write (untOut,trim(fmt)) x,fx
     endif
 endif
 ! Allocate MoveRate stuff
@@ -323,17 +345,17 @@ allocate(move(n))
 if(allocated(moverate)) deallocate(moverate)
 allocate(moverate(n))
 
-!ok = Rinit();ok=Reval('X11()')
-
-again=.true.;compt=0;!ok=Reval("mrl<-c()")
+again=.true.;compt=0
+if(present(MonitorFile)) call writeMonitor(untMonitor,nAdapt*compt,nAdapt*nCycles)
 do while(again)
     !Start sampling
     moverate=0._mrk
     do i=1,nAdapt
         call Metro_OAAT_GaussJump(f,x,fx,fAux,std,move,err,mess)
         If(err>0) then
-            mess='Adaptive_Metro_OAAT'//trim(mess)
-            if(present(OutFile)) close(1)
+            mess='Adaptive_Metro_OAAT:'//trim(mess)
+            if(present(OutFile)) close(untOut)
+            if(present(MonitorFile)) close(untMonitor)
             return
         endif
         postlist(i)=fx
@@ -341,20 +363,15 @@ do while(again)
         if(present(OutFile)) then
             if(present(fAux)) then
                 fmt='(' // trim(number_string(n+1+p)) // 'e14.6)'
-                write (1,trim(fmt)) x,fx,fAux
+                write (untOut,trim(fmt)) x,fx,fAux
             else
                 fmt='(' // trim(number_string(n+1)) // 'e14.6)'
-                write (1,trim(fmt)) x,fx
+                write (untOut,trim(fmt)) x,fx
             endif
         endif
         !Update Move Rates
         where(move) moverate=moverate+1._mrk/nAdapt
     enddo
-!    ok=Reval('layout(c(1, 2))')
-!    ok=Rput('mr', moverate)
-!    ok=Reval('matplot(mr,type="l", xlab="Parameter", ylab="Move Rate", main="Move Rates", ylim=c(0, 1))')
-!    ok=Rput('post', postlist)
-!    ok=Reval('matplot(post,type="l", xlab="Iteration", ylab="Log-posterior", main="Log-posterior")')
 
     ! Adapt Jump standard deviation
     where(moverate<=MinMoveRate) std=std*DownMult
@@ -366,12 +383,24 @@ do while(again)
         write(*,*) 'Again?'
         read(*, *) again
     endif
+    if(present(MonitorFile)) call writeMonitor(untMonitor,nAdapt*compt,nAdapt*nCycles)
 enddo
 
-if(present(OutFile)) close(1)
-!ok = Rclose()  ! End R session
+if(present(OutFile)) close(untOut)
+if(present(MonitorFile)) close(untMonitor)
 
 end subroutine Adaptive_Metro_OAAT
+
+!*********!
+! PRIVATE !
+!*********!
+
+subroutine writeMonitor(unt,i,n)
+use utilities_dmsl_kit, only:number_string
+integer(mik),intent(in)::unt,i,n
+rewind(unt)
+write (unt,'(A)') trim(number_string(i))//'/'//trim(number_string(n))
+end subroutine
 
 
 end module MCMCStrategy_tools

e995d5c8ce2aaf69ce6b876487f0e50b1d280558
benRenard <benjamin.renard@inrae.fr>
2022-08-03 17:35:56 +0200

    Geodesy: new subs to handle raster data * new interfaces for distance computations * utilities for manipulating raster grids * IDW-interpolation on raster grid

--- src/Geodesy_tools.f90
+++ src/Geodesy_tools.f90
@@ -1,11 +1,12 @@
 module Geodesy_tools
 
 !~**********************************************************************
-!~* Purpose: tools for geodesy, mostly distance computations 
+!~* Purpose: tools for geodesy, e.g. distance computations,
+!~*          raster grids, interpolation, etc.
 !~**********************************************************************
 !~* Programmer: Ben Renard, Irstea Lyon
 !~**********************************************************************
-!~* Created: 08/02/2017
+!~* Created: 08/02/2017 - Last Update: 03/08/2022
 !~**********************************************************************
 !~* Comments:
 !~**********************************************************************
@@ -14,20 +15,28 @@ module Geodesy_tools
 !~* 2Do List:
 !~**********************************************************************
 !~* Quick description of public procedures:
-!~*		1.
+!~*     1.
 !~**********************************************************************
 
 use kinds_dmsl_kit ! numeric kind definitions from DMSL
 
 implicit none
 Private
-public :: GetDistance
+public :: GetDistance, validateRasterGrid, rasterToPoints, interpolateOnGrid
+
+type, public:: rasterGridType
+    integer(mik) :: ncols=-99,nrows=-99
+    character(250)::name='AintGotNoName'
+    real(mrk)::xllcorner=undefRN, yllcorner=undefRN, cellsize=undefRN
+    real(mrk) :: mv=undefRN ! missing values code
+end type rasterGridType
 
 interface GetDistance
-  module procedure GetDistance_2,GetDistance_n
+  module procedure GetDistance_2,GetDistance_1n,GetDistance_1grid,GetDistance_n
 endinterface GetDistance
 
 contains
+
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 subroutine GetDistance_2(formula,pt1,pt2,par,cov1,cov2,d,err,mess)
 !^**********************************************************************
@@ -44,16 +53,16 @@ subroutine GetDistance_2(formula,pt1,pt2,par,cov1,cov2,d,err,mess)
 !^* 2Do List:
 !^**********************************************************************
 !^* IN
-!^*		1.formula
-!^*		2.pt1
-!^*		3.pt2
-!^*		4.[par] optional parameter vector 
-!^*		5.[cov1] optional covariate vector at site 1
-!^*		6.[cov2] optional covariate vector at site 2
+!^*     1.formula
+!^*     2.pt1
+!^*     3.pt2
+!^*     4.[par] optional parameter vector
+!^*     5.[cov1] optional covariate vector at site 1
+!^*     6.[cov2] optional covariate vector at site 2
 !^* OUT
-!^*		1.d, distance
-!^*		2.err, error code; <0:Warning, ==0:OK, >0: Error
-!^*		3.mess, error message
+!^*     1.d, distance
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
 !^**********************************************************************
 character(*),intent(in)::formula
 real(mrk),intent(in)::pt1(:),pt2(:)
@@ -85,6 +94,121 @@ case default
 end select
 
 end subroutine GetDistance_2
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subroutine GetDistance_1n(formula,pt1,pts,par,cov1,cov,D,err,mess)
+!^**********************************************************************
+!^* Purpose: Compute distance vector between one and n points
+!^**********************************************************************
+!^* Programmer: Ben Renard, INRAE Aix
+!^**********************************************************************
+!^* Created: 03/08/2022
+!^**********************************************************************
+!^* Comments:
+!^**********************************************************************
+!^* References:
+!^**********************************************************************
+!^* 2Do List:
+!^**********************************************************************
+!^* IN
+!^*     1.formula
+!^*     2.pt1, single point
+!^*     3.pts, n points
+!^*     4.[par] optional parameter vector
+!^*     5.[cov1] optional covariate vector for single site
+!^*     6.[cov] optional covariate matrix (row: sites, columns: covariates)
+!^* OUT
+!^*     1.D, distance vector
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
+!^**********************************************************************
+character(*),intent(in)::formula
+real(mrk),intent(in)::pt1(:),pts(:,:)
+real(mrk),intent(in),optional::par(:),cov1(:),cov(:,:)
+real(mrk),intent(out)::D(:)
+integer(mik),intent(out)::err
+character(*),intent(out)::mess
+! locals
+character(250),parameter::procname='GetDistance_1n'
+integer(mik)::n,i
+
+err=0;mess='';D=0._mrk
+
+n=size(pts,dim=1)
+if(size(D)/=n) then
+    err=1;mess=trim(procname)//':Fatal:incorrect size [D]';return
+endif
+do i=1,n
+    if(present(cov1) .and. present(cov) .and. present(par)) then
+        call GetDistance_2(formula=formula,pt1=pt1,pt2=pts(i,:),&
+                           par=par,cov1=cov1,cov2=cov(i,:),&
+                           d=D(i),err=err,mess=mess)
+    else
+        call GetDistance_2(formula=formula,pt1=pt1,pt2=pts(i,:),&
+                           d=D(i),err=err,mess=mess)
+    endif
+    if(err/=0) then;mess=trim(procname)//':'//trim(mess);return;endif
+enddo
+
+end subroutine GetDistance_1n
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subroutine GetDistance_1grid(formula,pt1,grid,D,err,mess)
+!^**********************************************************************
+!^* Purpose: Compute distance vector between one point and a grid
+!^**********************************************************************
+!^* Programmer: Ben Renard, INRAE Aix
+!^**********************************************************************
+!^* Created: 03/08/2022
+!^**********************************************************************
+!^* Comments:
+!^**********************************************************************
+!^* References:
+!^**********************************************************************
+!^* 2Do List:
+!^**********************************************************************
+!^* IN
+!^*     1.formula
+!^*     2.pt1, single point
+!^*     3.grid, grid on which interpolated values should be computed (rasterGrid object)
+!^* OUT
+!^*     1.D, distance matrix
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
+!^**********************************************************************
+character(*),intent(in)::formula
+real(mrk),intent(in)::pt1(:)
+type(rasterGridType), intent(in)::grid
+real(mrk),intent(out)::D(:,:)
+integer(mik),intent(out)::err
+character(*),intent(out)::mess
+! locals
+character(250),parameter::procname='GetDistance_1grid'
+integer(mik)::i,j
+real(mrk)::pt2(2)
+
+err=0;mess='';D=undefRN
+
+! Validate grid
+call validateRasterGrid(grid,err,mess)
+if(err>0) then;mess=trim(procname)//':'//trim(mess);return;endif
+! Validate D
+if(size(D,dim=1)/=grid%nrows .or. size(D,dim=2)/=grid%ncols) then
+    err=1;mess=trim(procname)//':Fatal:size mismatch [D,grid]';return
+endif
+
+do i=1,grid%nrows
+    do j=1,grid%ncols
+        pt2(1)=grid%xllcorner+real(j-0.5,mrk)*grid%cellsize
+        pt2(2)=grid%yllcorner+real(i-0.5,mrk)*grid%cellsize
+        call GetDistance_2(formula=formula,pt1=pt1,pt2=pt2,&
+                           D=D(grid%nrows-i+1,j),err=err,mess=mess)
+        if(err/=0) then;mess=trim(procname)//':'//trim(mess);return;endif
+    enddo
+enddo
+
+end subroutine GetDistance_1grid
+
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 subroutine GetDistance_n(formula,pts,par,cov,D,err,mess)
 !^**********************************************************************
@@ -101,14 +225,14 @@ subroutine GetDistance_n(formula,pts,par,cov,D,err,mess)
 !^* 2Do List:
 !^**********************************************************************
 !^* IN
-!^*		1.formula
-!^*		2.pts
-!^*		3.[par] optional parameter vector 
-!^*		4.[cov] optional covariate matrix (row: sites, columns: covariates)
+!^*     1.formula
+!^*     2.pts
+!^*     3.[par] optional parameter vector
+!^*     4.[cov] optional covariate matrix (row: sites, columns: covariates)
 !^* OUT
-!^*		1.D, distance matrix
-!^*		2.err, error code; <0:Warning, ==0:OK, >0: Error
-!^*		3.mess, error message
+!^*     1.D, distance matrix
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
 !^**********************************************************************
 character(*),intent(in)::formula
 real(mrk),intent(in)::pts(:,:)
@@ -143,5 +267,188 @@ do i=2,n
 enddo
 
 end subroutine GetDistance_n
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subroutine validateRasterGrid(grid,err,mess)
+!^**********************************************************************
+!^* Purpose: Validate raster grid object.
+!^**********************************************************************
+!^* Programmer: Ben Renard, INRAE Aix
+!^**********************************************************************
+!^* Created: 03/08/2022
+!^**********************************************************************
+!^* Comments:
+!^**********************************************************************
+!^* References:
+!^**********************************************************************
+!^* 2Do List:
+!^**********************************************************************
+!^* IN
+!^*    1. grid, grid definition (rasterGrid object)
+!^* OUT
+!^*    1.err, error code; ==0:OK, >0: Error
+!^*    2.mess, error message
+!^**********************************************************************
+type(rasterGridType), intent(in)::grid
+integer(mik),intent(out)::err
+character(*),intent(out)::mess
+! locals
+character(250),parameter::procname='validateRasterGrid'
+logical::ncolOK,nrowOK,sizeOK
+
+err=0;mess=''
+
+ncolOK=(grid%ncols>0)
+nrowOK=(grid%nrows>0)
+sizeOK=(grid%cellsize>0)
+
+if(ncolOK .and. nrowOK .and. sizeOK) then
+    mess='ok'
+else
+    err=1;mess=trim(procname)//':FATAL'
+    if(.not. ncolOK) mess=trim(mess)//' - invalid ncols'
+    if(.not. nrowOK) mess=trim(mess)//' - invalid nrows'
+    if(.not. sizeOK) mess=trim(mess)//' - invalid cellsize'
+endif
+
+end subroutine validateRasterGrid
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subroutine rasterToPoints(grid,M,pts,err,mess)
+!^**********************************************************************
+!^* Purpose: Transform a raster dataset in long matrix format with
+!^*          columns (x,y,val).
+!^**********************************************************************
+!^* Programmer: Ben Renard, INRAE Aix
+!^**********************************************************************
+!^* Created: 03/08/2022
+!^**********************************************************************
+!^* Comments:
+!^**********************************************************************
+!^* References:
+!^**********************************************************************
+!^* 2Do List:
+!^**********************************************************************
+!^* IN
+!^*    1. grid, grid definition (rasterGrid object)
+!^*    2. [M], data matrix. If not present, third column of "pts" is filled with junk.
+!^* OUT
+!^*     1.points, (nrow*ncol) x 3 array with columns (x,y,val)
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
+!^**********************************************************************
+type(rasterGridType), intent(in)::grid
+real(mrk),intent(in),optional::M(:,:)
+real(mrk),allocatable,intent(out)::pts(:,:)
+integer(mik),intent(out)::err
+character(*),intent(out)::mess
+! locals
+character(250),parameter::procname='rasterToPoints'
+integer(mik)::i,j,k
+
+err=0;mess=''
+
+call validateRasterGrid(grid,err,mess)
+if(err>0) then;mess=trim(procname)//':'//trim(mess);return;endif
+
+allocate(pts(grid%ncols*grid%nrows,3))
+pts=undefRN;k=0
+do i=1,grid%nrows
+    do j=1,grid%ncols
+        k=k+1
+        pts(k,1)=grid%xllcorner+real(j-0.5,mrk)*grid%cellsize
+        pts(k,2)=grid%yllcorner+real(i-0.5,mrk)*grid%cellsize
+        if(present(M)) pts(k,3)=M(grid%nrows-i+1,j)
+    enddo
+enddo
+
+end subroutine rasterToPoints
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subroutine interpolateOnGrid(pts,grid,method,par,M,err,mess)
+!^**********************************************************************
+!^* Purpose: Interpolate a set of (x,y,value) points on a regular grid
+!^**********************************************************************
+!^* Programmer: Ben Renard, INRAE Aix
+!^**********************************************************************
+!^* Created: 03/08/2022
+!^**********************************************************************
+!^* Comments: For inverse-distance weighting, zero-distances are replaced
+!^*           by a small number (1/1000000 of grid resolution).
+!^**********************************************************************
+!^* References:
+!^**********************************************************************
+!^* 2Do List:
+!^**********************************************************************
+!^* IN
+!^*     1. pts, n*3 array with columns (x,y,value) to be interpolated
+!^*     2. grid, grid on which interpolated values should be computed (rasterGrid object)
+!^*     3. method, interpolation method
+!^*     4.[par] optional parameter vector
+!^* OUT
+!^*     1.M, array containing interpolated values
+!^*     2.err, error code; <0:Warning, ==0:OK, >0: Error
+!^*     3.mess, error message
+!^**********************************************************************
+use utilities_dmsl_kit, only:number_string,changeCase
+real(mrk),intent(in)::pts(:,:)
+type(rasterGridType), intent(in)::grid
+character(*),intent(in)::method
+real(mrk),intent(in),optional::par(:)
+real(mrk), allocatable, intent(out)::M(:,:)
+integer(mik),intent(out)::err
+character(*),intent(out)::mess
+! locals
+character(250),parameter::procname='interpolateOnGrid'
+real(mrk),parameter::IDWpar_def=1._mrk
+integer(mik)::n,p,i
+real(mrk),allocatable::distances(:,:,:),sumDist(:,:),weights(:,:,:)
+real(mrk)::smallDist,pow
+
+err=0;mess=''
+
+! Validate grid
+call validateRasterGrid(grid,err,mess)
+if(err>0) then;mess=trim(procname)//':'//trim(mess);return;endif
+! Validate points to be interpolated
+n=size(pts,dim=1);p=size(pts,dim=2)
+if(n<1) then
+    mess=trim(procname)//':[points] should have at least 1 row: n='//trim(number_string(n))
+    err=1;return
+endif
+if(p<3) then
+    mess=trim(procname)//':[points] should have at least 3 columns: p='//trim(number_string(p))
+    err=1;return
+endif
+
+allocate(M(grid%nrows,grid%ncols))
+M=0._mrk
+
+select case(changeCase(trim(method),'lower'))
+case('inversedistance','inversedistanceweighting','idw')
+    ! handle parameter
+    if(present(par)) then;pow=par(1);else;pow=IDWpar_def;endif
+    ! Compute distance between each point and grid
+    allocate(distances(grid%nrows,grid%ncols,n),weights(grid%nrows,grid%ncols,n))
+    allocate(sumDist(grid%nrows,grid%ncols))
+    smallDist=grid%cellsize/1000000._mrk
+    do i=1,n
+        call GetDistance(formula='euclidean',pt1=pts(i,:),grid=grid,&
+                         D=distances(:,:,i),err=err,mess=mess)
+        if(err>0) then;mess=trim(procname)//':'//trim(mess);return;endif
+        where(distances(:,:,i)<=0._mrk) distances(:,:,i)=smallDist
+    enddo
+    ! Deduce weights
+    sumDist=sum(1._mrk/distances**pow,dim=3)
+    do i=1,n
+        weights(:,:,i)=(1._mrk/distances(:,:,i)**pow)/sumDist
+        M=M+weights(:,:,i)*pts(i,3)
+    enddo
+case default
+    err=1;mess=trim(procname)//':Fatal:Unavailable method';return
+end select
+
+end subroutine interpolateOnGrid
+
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end module Geodesy_tools
